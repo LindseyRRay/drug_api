409f170731d41aa4e9d56dd22486dc9075d3c015
lrraymond13@gmail.com <lrraymond13@gmail.com>
2016-09-27 10:39:56 -0400

    added file download

--- api_access.py
+++ api_access.py
@@ -2,9 +2,12 @@ import funcy
 import multiprocessing
 import requests
 from requests.auth import HTTPDigestAuth
+import pandas as pd
+import numpy as np
 from xml.etree import ElementTree
+import os
 
-from dev import TRIAL_ID_URL, AUTH_USER, AUTH_PASS
+from dev import TRIAL_ID_URL, TRIAL_INFO_URL, AUTH_USER, AUTH_PASS, TRIAL_XML_DIR
 
 
 def parse_xml_for_trial_id(res_content):
@@ -57,3 +60,68 @@ def batch_process_requests():
     results = pool.map(paginate_trial_ids, xrange(0, 250000, 50000))
     print len(results)
     return results
+
+
+def compile_ids_output(request_res, filename=None, directory=None):
+    # collect id values and output to csv
+    ids = []
+    for process_res in request_res:
+        for (code, offset, res) in process_res:
+            if code == requests.code.ok:
+                ids.append(res)
+    ser = pd.Series(np.array(funcy.flatten(ids)), name='TrialId')
+    path = 'TrialIds.csv' or filename
+    if directory:
+        path = os.path.join(directory, path)
+    ser.to_csv(path, index=False)
+
+
+def store_xml(trial_id, res_content, DIR=TRIAL_XML_DIR):
+    # parse and store trial xml
+    # save content as xml tree
+    tree = ElementTree.fromstring(res_content)
+    # convert to a tree
+    t2 = ElementTree.ElementTree(tree)
+    # write to .xml
+    with open(os.path.join(DIR, '{}.xml'.format(trial_id)), 'w') as f:
+        t2.write(f)
+
+
+def search_trial_id(trial_id):
+    # get trial information for each trial id
+    params = {'idList': trial_id}
+    res = requests.get(TRIAL_INFO_URL, auth=HTTPDigestAuth(AUTH_USER, AUTH_PASS), params=params)
+    if res.status_code != requests.codes.ok:
+        print res.status_code
+        return (res.status_code, trial_id, None)
+    # get xml info and save
+    return (res.status_code, trial_id, store_xml(trial_id, res.content))
+
+
+def get_trial_info(trial_ids):
+    # map the search trial id function to a list of ids
+    res = map(search_trial_id, trial_ids)
+
+
+def read_trial_ids(filename=None):
+    # read a previously downloads csv of trial ids
+    if not filename:
+        filename = 'TrialIds.csv'
+    trial_ids = pd.read_csv(os.path.join(TRIAL_XML_DIR, filename))
+    trial_ids_arr = trial_ids.TrialId.values
+    print len(trial_ids_arr)
+    return trial_ids_arr
+
+
+def batch_process_xml_download(trial_ids):
+    # set up multiprocess pool
+    # create pool of processes
+    pool = multiprocessing.Pool(processes=10)
+    chunk_size = int(len(trial_ids)/10)+1
+    print chunk_size
+    results = pool.map(get_trial_info, funcy.chunks(chunk_size, trial_ids))
+    print len(results)
+    return results
+
+
+
